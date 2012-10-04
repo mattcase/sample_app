8d37a8e81fbcccbff777e77960f4036ffa1a1e34
Matt Case <matt.case@gmail.com>
2012-10-03 19:49:15 -0500

    Fixed sign up page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,21 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', :text => "#{base_title} | About Us"
+    click_link "Help"
+    page.should have_selector 'title', :text => "#{base_title} | Help"
+    click_link "Contact"
+    page.should have_selector 'title', :text => "#{base_title} | Contact"
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', :text => "#{base_title} | Sign up"
+    #click_link "sample app"
+    #page.should have_selector 'title', :text => "#{base_title} |"
+  end
+
   subject { page }
 
   describe "Home page" do
@@ -35,4 +50,5 @@ describe "Static pages" do
     it { should have_selector('h1', :text => 'Contact') }
     it { should have_selector('title', :text => "#{base_title} | Contact") }
   end
+
 end
